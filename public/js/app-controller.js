09eae93b19fd4a1d49c8a2d6bf7f7d527c0afddb
muhamedhas <hasanspahic.muhamed@gmail.com>
2018-05-30 22:33:44 +0200

    app-conrollers

--- public/js/app-controller.js
+++ public/js/app-controller.js
@@ -1,4 +1,21 @@
-function AppController($scope, $http){
-    console.log('Hello from app controller');
-    
-}
+var app = angular.module('app', ['serviceModule', 'controllerModule', 'customerModule', 'ngRoute'])
+
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when("/", {
+			templateUrl: "../pages/home.html"
+		})
+		.when("/restaurant", {
+			templateUrl: "../pages/restaurants.html",
+			controller: 'restaurantCtrl'
+
+		})
+		.when("/reservation", {
+			templateUrl: "../pages/reservation.html",
+			controller: 'customerCtrl'
+		})
+		.when('/editrestaurant', {
+			templateUrl: "../pages/editRestaurant.html"
+		})
+})
