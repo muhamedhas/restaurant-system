162fbdebb987688e7d51c833cde6f722f5000479
muhamedhas <hasanspahic.muhamed@gmail.com>
2018-05-31 09:10:10 +0200

    serve home page

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ let express = require('express')
 let app = express()
 let bodyParser = require('body-parser')
 
-//var constants = require('./constants')
+var constants = require('./constants')
 
 // db dependencies
 var MongoClient = require('mongodb').MongoClient
 var ObjectId = require('mongodb').ObjectID
-//var url = constants.DB_URL
+var url = constants.DB_URL
 
-//var query = require('./query')
+var query = require('./query')
 
 app.use(bodyParser.json())
 app.use(express.static('../public'))
@@ -20,8 +20,17 @@ app.get('/', function (req, res) {
 	res.send('index.html')
 })
 
+// find restaurant api
+app.get('/restaurants/:_id?', (req, res) => {
+	console.log(req.params)
 
-// listen
-app.listen(3000, function () {
-	console.log('Server is running')
+	var criteria = (req.params) ? { "_id": ObjectId(req.params['_id']) } : ''
+	console.log(criteria)
+	console.log('restaurants api reached.. ')
+	MongoClient.connect(url, function (err, db) {
+		// assert.equal(null, err);
+		query.findRestaurants(req, res, db, function () {
+			db.close()
+		})
+	})
 })
